fa6dc24c242c181cd2076b5a3c75b372ef7bcd36
Perugius <g.alyn97@gmail.com>
2017-04-29 18:16:24 +0300

    Imbunatatiri UI Settings

--- src/Settings.h
+++ src/Settings.h
@@ -11,7 +11,7 @@
 #include <QLineEdit>
 #include <QPushButton>
 #include <QToolButton>
-#include <QVBoxLayout>
+#include <QGridLayout>
 #include <QWidget>
 #include "src/BrowserButton.h"
 #include "src/Constants.h"
@@ -31,64 +31,48 @@ public:
     //...
 
 private:
+    QWidget *bonus;
     QWidget *defaultButtons;
     QWidget *pythonButtons;
     QPushButton *saveButtonDefault;
     QPushButton *defaultsButtonDefault;
     QPushButton *saveButtonPython;
     QPushButton *defaultsButtonPython;
-    QHBoxLayout *pythonButtonHorizontalLayout;
-    QHBoxLayout *defaultButtonHorizontalLayout;
     QGroupBox *defaultPaths;
     QGroupBox *pythonScripts;
-    QLayout *layoutSettings;
+    QVBoxLayout *layoutSettings;
     QWidget *database;
     QLabel *databasePathsLabel;
     QLineEdit *databasePaths;
     BrowserButton *databaseBrowser;
-    QHBoxLayout *databaseHorizontalLayout;
-    QWidget *reader;
-    QLabel *readerPathsLabel;
-    QLineEdit *readerPaths;
-    BrowserButton *readerBrowser;
-    QHBoxLayout *readerHorizontalLayout;
     QWidget *scanner;
     QLabel *scannerPathsLabel;
     QLineEdit *scannerPaths;
     BrowserButton *scannerBrowser;
-    QHBoxLayout *scannerHorizontalLayout;
-    QWidget *settings;
-    QLabel *settingsPathsLabel;
-    QLineEdit *settingsPaths;
-    BrowserButton *settingsBrowser;
-    QHBoxLayout *settingsHorizontalLayout;
     QWidget *bookSync;
     QLabel *bookSyncPathsLabel;
     QLineEdit *bookSyncPaths;
     BrowserButton *bookSyncBrowser;
-    QHBoxLayout *bookSyncHorizontalLayout;
     QWidget *translator;
     QLabel *translatorPathsLabel;
     QLineEdit *translatorPaths;
     BrowserButton *translatorBrowser;
-    QHBoxLayout *translatorHorizontalLayout;
     QWidget *dictionary;
     QLabel *dictionaryPathsLabel;
     QLineEdit *dictionaryPaths;
     BrowserButton *dictionaryBrowser;
-    QHBoxLayout *dictionaryHorizontalLayout;
-    QWidget *worldSearch;
-    QLabel *worldSearchPathsLabel;
-    QLineEdit *worldSearchPaths;
-    BrowserButton *worldSearchBrowser;
-    QHBoxLayout *worldSearchHorizontalLayout;
     QWidget *authorSync;
     QLabel *authorSyncPathsLabel;
     QLineEdit *authorSyncPaths;
     BrowserButton *authorSyncBrowser;
-    QHBoxLayout *authorSyncHorizontalLayout;
-    QVBoxLayout *defaultVerticalLayout;
-    QVBoxLayout *pythonVerticalLayout;
+    QGridLayout *defaultLayout;
+    QGridLayout *pythonLayout;
+    QPushButton *databaseDefault;
+    QPushButton *scannerDefault;
+    QPushButton *bookSyncDefault;
+    QPushButton *authorSyncDefault;
+    QPushButton *dictionaryDefault;
+    QPushButton *translatorDefault;
 };
 #endif // SETTINGS
 
