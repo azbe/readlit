87308699c598400630eca655732d05d99ca45e2f
azbe <tudorpetreradu@gmai.com>
2017-04-30 22:31:14 +0300

    .

--- sync_goodreads.py
+++ sync_goodreads.py
@@ -8,16 +8,22 @@ if len(sys.argv) != 2:
 	sys.exit(1)
 searchTitle = re.sub(r'[^ \w]', '', ('goodreads ' + sys.argv[1]).lower())
 
-urls = search(searchTitle, tld='com', lang='en', stop=5)
-url = next(urls, None)
+try:
+	urls = search(searchTitle, tld='com', lang='en', stop=5)
+	url = next(urls, None)
 
-page = requests.get(url)
-tree = html.fromstring(page.content)
+	page = requests.get(url)
+	tree = html.fromstring(page.content)
 
-title = re.sub(r'(^([ \n]*))|(\n)', '', tree.xpath('//h1[@id="bookTitle"]/text()')[0])
-author = re.sub(r'(^([ \n]*))|(\n)', '', tree.xpath('//div[@id="bookAuthors"]/span[@itemprop="author"]/a[@class="authorName"]/span[@itemprop="name"]/text()')[0])
-description = re.sub(r'(&#[0-9]*;?)|(<br/?>)|(</?[bi]>)|(</?span[^>]*>)|(\n*$)', '', etree.tostring(tree.xpath('//div[@id="description"]/span')[1], pretty_print=True))
+	title = re.sub(r'(^([ \n]*))|(\n)', '', tree.xpath('//h1[@id="bookTitle"]/text()')[0])
+	author = re.sub(r'(^([ \n]*))|(\n)', '', tree.xpath('//div[@id="bookAuthors"]/span[@itemprop="author"]/a[@class="authorName"]/span[@itemprop="name"]/text()')[0])
+	description = re.sub(r'(&#[0-9]*;?)|(<br/?>)|(</?[bi]>)|(</?span[^>]*>)|(\n*$)', '', etree.tostring(tree.xpath('//div[@id="description"]/span')[1], pretty_print=True))
 
-print 'Book Title: ', title
-print 'Book Author: ', author
-print 'Book Description: ', description
+	print title
+	print author
+	print description
+except:
+	print 'ERROR'
+	print 'ERROR'
+	print 'ERROR'
+
