5353793acbb14a3a52606819aca35d0d687d89f4
azbe <tudorpetreradu@gmai.com>
2017-06-05 15:42:13 +0300

    Fixed authors.

--- sync_author.py
+++ sync_author.py
@@ -14,11 +14,24 @@ url = next(urls, None)
 page = requests.get(url)
 tree = html.fromstring(page.content)
 
-name = re.sub(r'(^([ \n]*))|(\n)', '', tree.xpath('//h1[@class="authorName"]/span[@itemprop="name"]/text()')[0])
-birth = re.sub(r'(^([ \n]*))|(\n)', '', tree.xpath('//div[@class="dataItem"][@itemprop="birthDate"]/text()')[0])
-death = re.sub(r'(^([ \n]*))|(\n)', '', tree.xpath('//div[@class="dataItem"][@itemprop="deathDate"]/text()')[0])
-descriptionTree = tree.xpath('//div[@class="aboutAuthorInfo"]/span')
-description = re.sub(r'(&#[0-9]*;?)|(\n*<br/?>\n*)|(\n*</?[bi]>\n*)|(</?span[^>]*>)|(</?a[^>]*>)|(\n*$)', '', etree.tostring(descriptionTree[len(descriptionTree) - 1], pretty_print=True).decode('utf-8'))
+treeName = tree.xpath('//h1[@class="authorName"]/span[@itemprop="name"]/text()')
+treeBirth = tree.xpath('//div[@class="dataItem"][@itemprop="birthDate"]/text()')
+treeDeath = tree.xpath('//div[@class="dataItem"][@itemprop="deathDate"]/text()')
+treeDescription = tree.xpath('//div[@class="aboutAuthorInfo"]/span')
+
+name = re.sub(r'(^([ \n]*))|(\n)', '', treeName[0])
+if treeBirth:
+	birth = re.sub(r'(^([ \n]*))|(\n)', '', treeBirth[0])
+else:
+	birth = 'n/a'
+if treeDeath:
+	death = re.sub(r'(^([ \n]*))|(\n)', '', treeDeath[0])
+else:
+	death = 'n/a'
+if treeDescription:
+	description = re.sub(r'(&#[0-9]*;?)|(\n*<br/?>\n*)|(\n*</?[bi]>\n*)|(</?span[^>]*>)|(</?a[^>]*>)|(\n*$)', '', etree.tostring(treeDescription[len(treeDescription) - 1], pretty_print=True).decode('utf-8'))
+else:
+	description = 'n/a'
 
 print(name)
 print(birth)
